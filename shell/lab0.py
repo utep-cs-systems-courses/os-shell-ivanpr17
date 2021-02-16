9eb424e9ebf99260cf7ea8cb704c57c6efbdd74e
ivanpr17 <ivanpr17l@gmail.com>
2021-02-15 20:21:45 -0700

    Updated Lab0

---
+++ shell/lab0.py
@@ -0,0 +1,35 @@
+from os import read
+
+next = 0
+limit = 0
+
+def my_getLine():
+    global next
+    global limit
+    line = ""
+    char = my_getChar()
+    while (char!= '' and char != "EOF"):
+        line += char
+        char = my_getChar()
+
+    next = 0
+    limit = 0
+    return line
+
+def my_getChar():
+    global next
+    global limit
+
+    if next == limit:
+        next = 0
+        limit = read(0,100)
+        if limit == 0:
+            return "EOF"
+
+    if next < len(limit) -1:
+        c = chr(limit[next])
+        next +=1
+        return c
+
+    else:
+        return "EOF"
